81d008f21aeb3d90b1be37c0816eed68171370e9
Gabriel Cholette-Rioux <Showlet95@hotmail.com>
2018-05-09 22:40:14 -0400

    heyy

--- src/Main.elm
+++ src/Main.elm
@@ -2,19 +2,40 @@ import Html exposing (Html, text, div, button)
 import Html.Events exposing (..)
 import Http exposing (..)
 import HttpBuilder exposing (..)
+import Json.Decode as Decode exposing (Decoder)
+import Json.Decode.Pipeline exposing (decode, required, optional)
+
 
 type alias Model = String
 
-type Msg = BtnPress | HttpGET (Result Http.Error String)
+type Msg 
+    = BtnPress 
+    | HttpGET (Result Http.Error PageContent)
 
 
-get : Http.Request String 
+type alias PageContent =
+    { kind : String
+    }
+
+
+get : Http.Request PageContent 
 get =
-    "http://localhost:3005/books"
+    "https://www.reddit.com/r/compsci/search.json?q=show"
         |> HttpBuilder.get
-        |> withExpect Http.expectString
+        |> withExpect (Http.expectJson decoder)
         |> HttpBuilder.toRequest
 
+
+
+decoder : Decoder PageContent
+decoder =
+    decode PageContent
+        |> required "kind" Decode.string
+
+
+listDecoder : Decoder (List PageContent)
+listDecoder =
+    Decode.list decoder
 
 
 main : Program Never Model Msg
@@ -45,8 +66,8 @@ update msg model =
             BtnPress ->
                 (model, httpget)
 
-            HttpGET (Ok str) -> 
-                (str, Cmd.none)
+            HttpGET (Ok content) -> 
+                ((toString content), Cmd.none)
 
             HttpGET (Err err) -> 
                 ((toString err), Cmd.none)
